ccd691bfe242b475742435fde769e539109e45ce
carsantir99 <carsantir@gmail.com>
2025-01-27 21:08:22 +0100

    Revert "#7 - Desarrollo de capa de aplicación e inclusión de Jacoco"
    This reverts commit 1cadc62f509e091d87587536db8e56a2e6a60808.

--- src/main/java/com/pruebabcnc/utils/CommonUtilities.java
+++ src/main/java/com/pruebabcnc/utils/CommonUtilities.java
@@ -1,19 +1,5 @@
 package com.pruebabcnc.utils;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import com.pruebabcnc.exceptions.DateParserException;
-
 public class CommonUtilities {
 	public static final String YYYY_MM_DD_HH_MM_SS_HYPHEN_SEPARATOR = "yyyy-MM-dd HH:mm:ss";
-
-	public static LocalDateTime parseFromString(String date, String format) throws DateParserException {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
-		try {
-			return LocalDateTime.parse(date, formatter);
-		}catch(Exception e) {
-			throw new DateParserException("La fecha introducida posee un formato incorrecto. Debe usar el formato yyyy-MM-dd HH:mm:ss");
-		}
-	}
 }
--- src/test/java/com/pruebabcnc/application/PricesApplicationTests.java
+++
@@ -1,5 +0,0 @@
-package com.pruebabcnc.application;
-
-public class PricesApplicationTests {
-
-}
