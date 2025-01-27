6a2126fb216dc71a072179894ee8953f381fee4b
carsantir99 <carsantir@gmail.com>
2025-01-27 21:06:18 +0100

    #7 - Desarrollo de capa de aplicación e inclusión de Jacoco

---
+++ src/main/java/com/pruebabcnc/application/useCases/GetPriceByProductIdBrandIdAndDateUseCase.java
@@ -0,0 +1,18 @@
+package com.pruebabcnc.application.useCases;
+
+import com.pruebabcnc.domain.model.Price;
+import com.pruebabcnc.domain.repository.PricesRepository;
+import com.pruebabcnc.exceptions.DateParserException;
+import com.pruebabcnc.utils.CommonUtilities;
+
+public class GetPriceByProductIdBrandIdAndDateUseCase {
+    private final PricesRepository repository;
+
+    public GetPriceByProductIdBrandIdAndDateUseCase(PricesRepository repository) {
+        this.repository = repository;
+    }
+
+    public Price execute(Integer productId, Integer brandId, String date) throws DateParserException {
+        return repository.findPriceByProductIdBrandIdAndDate(productId, brandId, CommonUtilities.parseFromString(date, CommonUtilities.YYYY_MM_DD_HH_MM_SS_HYPHEN_SEPARATOR));
+    }
+}
